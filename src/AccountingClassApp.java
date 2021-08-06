c5b39aa04abbfb04a8bd51e16225d4b15030340f
DESKTOP-7SVMBFP\zhxjf <bambwc20@naver.com>
2021-08-06 21:31:07 +0900

    인스턴스의 활용

--- src/AccountingClassApp.java
+++ src/AccountingClassApp.java
@@ -1,9 +1,9 @@
 class Accounting {
-    public static double valueOfSupply;
-    public static double vatRate;
-    public static double expenseRate;
+    public double valueOfSupply;
+    public double vatRate;
+    public double expenseRate;
 
-    public static void print() {
+    public void print() {
         System.out.println("Value of supply : " + valueOfSupply);
         System.out.println("VAT : " + getVAT());
         System.out.println("Total : " + getTotal());
@@ -14,31 +14,31 @@ class Accounting {
         System.out.println("Dividend 3 : " + getDiviend3());
     }
 
-    public static double getDiviend1() {
+    public double getDiviend1() {
         return getIncome() * 0.5;
     }
 
-    public static double getDiviend2() {
+    public double getDiviend2() {
         return getIncome() * 0.3;
     }
 
-    public static double getDiviend3() {
+    public double getDiviend3() {
         return getIncome() * 0.2;
     }
 
-    public static double getIncome() {
+    public double getIncome() {
         return valueOfSupply - getExpense();
     }
 
-    public static double getExpense() {
+    public double getExpense() {
         return valueOfSupply * expenseRate;
     }
 
-    public static double getTotal() {
+    public double getTotal() {
         return valueOfSupply + getVAT();
     }
 
-    public static double getVAT() {
+    public double getVAT() {
         return valueOfSupply * vatRate;
     }
 }
@@ -46,13 +46,26 @@ class Accounting {
 public class AccountingClassApp {
 
     public static void main(String[] args) {
-        Accounting.valueOfSupply = 10000.0;
-        Accounting.vatRate = 0.1;
-        Accounting.expenseRate = 0.3;
-        Accounting.print();
+        // Accounting.valueOfSupply = 10000.0;
+        // Accounting.vatRate = 0.1;
+        // Accounting.expenseRate = 0.3;
+        // Accounting.print();
         // 클래스를 쓰면 어디에 속해있는지 확실하게 알 수 있다.
         // anotherVariable = ...;
         // anotherMethod = ...;
+
+        Accounting Account1 = new Accounting();
+        Account1.valueOfSupply = 10000.0;
+        Account1.vatRate = 0.1;
+        Account1.expenseRate = 0.3;
+
+        Accounting Account2 = new Accounting();
+        Account2.valueOfSupply = 10000.0;
+        Account2.vatRate = 0.1;
+        Account2.expenseRate = 0.3;
+
+        Account1.print();
+        Account2.print();
     }
 
 }
